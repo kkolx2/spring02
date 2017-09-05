abf79a80429f0a193afca2185cc733498307073f
java <java@bigdata>
2017-09-05 16:17:35 +0900

    Spring Comment add

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -3,8 +3,9 @@ package com.example;
 public class HelloSpring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Spring!!!");
+		System.out.println("Spring의 2가지 추상화된 설계");
+		System.out.println("1. DI (Dependency Injection) 의존 주입");
+		System.out.println("2. AOP (Aspect Oriented Programming) 관점 지향 프로그래밍");
 	}
 
 }
