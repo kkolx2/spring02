12cf193c396123b2fd1780d49c2cd73985668e6d
java <java@bigdata>
2017-09-05 16:44:19 +0900

    spring comment add2

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -5,6 +5,7 @@ public class HelloSpring {
 	public static void main(String[] args) {
 		System.out.println("Spring의 2가지 추상화된 설계");
 		System.out.println("1. DI (Dependency Injection) 의존 주입");
+		System.out.println("   - Spring이 구현한 구체적인 IoC");
 		System.out.println("2. AOP (Aspect Oriented Programming) 관점 지향 프로그래밍");
 	}
 
